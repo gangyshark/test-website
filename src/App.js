1eb1d33b3ca4664b262eb80e472ff178c4a6b8b1
cvharris <charris1014@gmail.com>
2018-05-18 21:09:01 -0500

    created a todo app in react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: [
+        {
+          id: 0,
+          description: "a todo",
+          isComplete: false
+
+        },
+        {
+          id: 1,
+          description: "walk the dog",
+          isComplete: false
+          
+        },
+        {
+          id:2,
+          description: "feed the baby",
+          isComplete: false
+          
+        },
+        {
+          id:3,
+          description: "sleep",
+          isComplete: false
+          
+        }
+      ]
+    }
+this.handleClickingTodo = this.handleClickingTodo.bind(this)
+  }
+
+  handleClickingTodo(clickedTodo) { 
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        return clickedTodo.id === todo.id ? { ...todo, isComplete: !todo.isComplete} : todo
+      })
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +56,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul style={{ textAlign: "left" }}>
+          {this.state.todos.map((todo, i) => (
+            <li key={i} onClick={(e) => this.handleClickingTodo(todo)} className={todo.isComplete ? "is-complete" : ""}> {todo.description} </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
 export default App;
+
+
